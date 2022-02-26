9ef76b1bccf911ed1d8759e31b67e6e5548f9bff
Yu Shen <y.shen@ucla.edu>
2022-02-26 12:07:09 -0800

    Add signup/login page

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import SignupPage from "./pages/signup";
+import ChangingPassword from "./pages/change-password";
+import "./styles/index.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<SignupPage />} />
+          <Route exact path="/change_password" element={<ChangingPassword/>} />
+        </Routes>
+      </Router>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ Client/src/pages/signup.jsx
@@ -0,0 +1,231 @@
+import React, { Component } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Notification from "../components/Notification"
+import axios from 'axios'
+
+class SignupPage extends Component {
+    constructor() {
+        super()
+        this.state = {
+            userName: '',
+            phoneNumber: '',
+            password: '',
+            is_signup: true,
+            is_login: false,
+            login_success: false,
+            alert : false,
+            alertMessage : null,
+            alertType : null,
+        }
+        this.closeAlert = this.closeAlert.bind(this);
+    }
+
+    changeUserName = (event) => {
+        this.setState({
+            userName: event.target.value
+        })
+    }
+
+    changePhoneNumber = (event) => {
+        this.setState({
+            phoneNumber: event.target.value
+        })
+    }
+
+    changePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    onClick_signup = (event) => {
+        event.preventDefault()
+        document.getElementById("phoneNumber").classList.remove("hidden");
+        document.getElementById("changePassword").classList.add("hidden");
+        this.setState({ is_signup: true, is_login: false, message: "Please sign up." })
+    }
+
+    onClick_login = (event) => {
+        event.preventDefault()
+        document.getElementById("phoneNumber").classList.add("hidden");
+        document.getElementById("changePassword").classList.remove("hidden");
+        this.setState({ is_signup: false, is_login: true, message: "Please log in." })
+    }
+
+    usernameMatched = (response, registered) => {
+        if (this.state.is_signup) {
+            this.setState({ 
+                alert : true,
+                alertMessage : "Username exists. Please log in.",
+                alertType : "error"
+            });
+        }
+        else if (this.state.is_login) {
+            if (response.data.password === registered.password) {
+                this.setState({ 
+                    alert : true,
+                    alertMessage : "Log in successful.",
+                    alertType : "success",
+                    login_success: true
+                });
+                sessionStorage.setItem("username", this.state.userName)
+                window.location.href = "home"
+            }
+            else {
+                this.setState({ 
+                    alert : true,
+                    alertMessage : "Password incorrect.",
+                    alertType : "error"
+                });
+            }
+        }
+    }
+
+    usernameUnmatched = () => {
+        if (this.state.is_signup) {
+            if ((this.state.phoneNumber.replace(/[- ]/g,'')).length !== 10) {
+                this.setState({
+                    alert : true,
+                    alertMessage : "Phone number must be 10-digit long.",
+                    alertType : "error",
+                })
+            } 
+            else if (isNaN((this.state.phoneNumber).replace(/[- ]/g,''))){
+                this.setState({
+                    alert : true,
+                    alertMessage : "Phone number must be all numbers.",
+                    alertType : "error",
+                })
+            }
+            else {
+                const new_user = {
+                    userName: this.state.userName,
+                    phoneNumber: (this.state.phoneNumber).replace(/[- ]/g,''),
+                    password: this.state.password,
+                    rating: 5,
+                    finished_order_number: 0,
+                }
+                axios.post("http://localhost:4000/app/signup", new_user)
+                    .then(response => console.log(response.data))
+                this.setState({ 
+                    alert : true,
+                    alertMessage : "Sign up successful. Please log in.",
+                    alertType : "success"
+                });
+            }
+        }
+        else if (this.state.is_login) {
+            this.setState({ 
+                alert : true,
+                alertMessage : "Username not registered.",
+                alertType : "error"
+            });
+        }
+    }
+
+    processResponse = (response, registered) => {
+        console.log("response.data: ", response.data)
+        if (response.data && response.data.userName === registered.userName) {
+            this.usernameMatched(response,registered);
+        }
+        else {
+            this.usernameUnmatched();
+        }
+    }
+
+    containsBlank = (signup, login) => {
+        if (signup)
+            return this.state.userName === '' || this.state.phoneNumber === '' || this.state.password === '';
+        else if (login)
+            return this.state.userName === '' || this.state.password === '';
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault()
+        if (this.containsBlank(this.state.is_signup, this.state.is_login)) {
+            this.setState({ 
+                alert : true,
+                alertMessage : "Please fill out all fields.",
+                alertType : "error"
+            });
+            return
+        }
+        const registered = {
+            userName: this.state.userName,
+            phoneNumber: this.state.phoneNumber,
+            password: this.state.password,
+        }
+        axios.post("http://localhost:4000/app/usercheck", registered)
+            .then(response => {
+                if (response) {
+                    this.processResponse(response, registered);
+                }
+                this.setState({
+                    userName: '',
+                    phoneNumber: '',
+                    password: '',
+                })
+            })
+    }
+
+    closeAlert() {
+        this.setState({ alert : false });
+    }
+
+    render() {
+        var heading = this.state.is_signup ? 'Sign up' : 'Log in'
+        return (
+            <div>
+                <Notification alert={this.state.alert} 
+                    alertMessage={this.state.alertMessage} 
+                    alertType={this.state.alertType} 
+                    closeAlert={this.closeAlert} />
+                <div className="heading">
+                    <p className='h1 text-center'> Bruin Market </p>
+                    <p className='h5 text-center'> Make the most out of your used goods! </p>
+                </div>
+                <div className='container'>
+                    <p className='h4 text-center'>{heading}</p>
+                    <div className='form-div'>
+                        <form onSubmit={this.onSubmit} className="marginForm">
+                            <input type='button' id='signup' onClick={this.onClick_signup} className='btn btn-block' value='Sign up' />
+                            <input type='button' id='login' onClick={this.onClick_login} className='btn btn-block' value='Log in' />
+                            <div>
+                                <input type='text'
+                                    placeholder='Username'
+                                    onChange={this.changeUserName}
+                                    value={this.state.userName}
+                                    className='form-control form-group signupInputText'
+                                />
+                            </div>
+                            <div id="phoneNumber">
+                                <input type='text'
+                                    placeholder='Phone Number'
+                                    onChange={this.changePhoneNumber}
+                                    value={this.state.phoneNumber}
+                                    className='form-control form-group signupInputText'
+                                />
+                            </div>
+                            <input type='password'
+                                placeholder='Password'
+                                onChange={this.changePassword}
+                                value={this.state.password}
+                                className='form-control form-group'
+                            />
+                            <input type='submit' className='btn btn-block' value='Submit' />
+                            <input type='submit' id="changePassword" onClick={
+                                (event) => {
+                                    event.preventDefault()
+                                    window.location.href = "change_password"
+                                }
+                            } className='btn btn-block hidden' value='Change Password' />
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default SignupPage
